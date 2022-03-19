dfa03361d3ab0998ccaa09ef1deac5fd017a125d
ftjuh <juh@posteo.org>
2022-03-19 15:49:27 +0100

    folders restructured

--- src/util/version.h
+++ src/util/version.h
@@ -2,8 +2,8 @@
 #define I2CwrapperVersion_h
 
 const uint8_t I2Cw_VersionMajor = 0;
-const uint8_t I2Cw_VersionMinor = 1;
-const uint8_t I2Cw_VersionPatch = 1;
+const uint8_t I2Cw_VersionMinor = 2;
+const uint8_t I2Cw_VersionPatch = 0;
 
 const uint32_t I2Cw_Version = (uint32_t)I2Cw_VersionMajor << 16 | (uint32_t)I2Cw_VersionMinor << 8 | (uint32_t)I2Cw_VersionPatch;
 
