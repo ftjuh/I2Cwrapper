814b84a46a09d978f284f338f90c6a2c0d520ac5
ftjuh <ftjuh@posteo.net>
2023-04-04 23:46:09 +0200

    added diagnostics option to Rotary_Encoder.ino example

--- examples/Rotary_Encoder/Rotary_Encoder.ino
+++ examples/Rotary_Encoder/Rotary_Encoder.ino
@@ -3,12 +3,14 @@
    SimplePollRotator.ino example
 
    Assumes a segmented disc with n phases and two light barrier switches,
-   shifted by a 1/4 phase against each other. Should work with other switches 
-   (Hall, mechanical, ...) as well, as long as both together generate a 
+   shifted by a 1/4 phase against each other. Should work with other switches
+   (Hall, mechanical, ...) as well, as long as both together generate a
    phase-shifted quadrature signal pattern.
 
    Pins configured for Quadrature Encoder for fischertechnik (https://www.thingiverse.com/thing:5867323)
    Change pins as needed. Note the comment on getDirection() and its altered behavior in I2Cwrapper context.
+
+   Uncomment the below #define DIAGNOSTICS_MODE to test diagnostics mode.
 
    (c) juh 2023
    based on SimplePollRotator.ino example from RotaryEncoder library (c) by Matthias Hertel, http://www.mathertel.de
@@ -19,6 +21,7 @@
 #include <Wire.h>
 #include <RotaryEncoderI2C.h>
 
+// #define DIAGNOSTICS_MODE // uncomment this for diagnostics mode demo
 
 #define PIN_IN1 4 // Attiny85 PB3, physical pin 3
 #define PIN_IN2 1 // Attiny85 PB1, physical pin 6
@@ -27,7 +30,7 @@
 uint8_t i2cAddress = 0x08;
 
 I2Cwrapper wrapper(i2cAddress); // each I2Cwrapper target device is represented by a wrapper...
-RotaryEncoderI2C encoder(&wrapper); // ...that the pin interface needs to communicate with the controller
+RotaryEncoderI2C encoder(&wrapper); // ...that the RotaryEncoder interface needs to communicate with the controller
 
 
 void setup()
@@ -54,6 +57,12 @@ void setup()
     halt("Error: Too many encoders attached to target.");
   }
 
+#ifdef DIAGNOSTICS_MODE
+  wrapper.setI2Cdelay(8); // We probably want the signal to come in fast, so lower this as much as possibile
+  encoder.startDiagnosticsMode(numEnc); // send pin states for this encoder; note there's no going back here, you'll have to reset the device to return to normal behavior
+  loopDiagnostics(); // will never return
+#endif // #ifdef DIAGNOSTICS_MODE
+
   Serial.println("SimplePollRotator example for I2CWrapper's RotaryEncoderI2C library.");
 
 }
@@ -66,7 +75,7 @@ void loop()
   // encoder.tick(); // not needed (in fact, not implemented). The target will do that for us.
 
   int newPos = encoder.getPosition(); // get the current position from I2C target
-  
+
   /* Note that if the target will execute tick() in its local loop (which is basically all
      it does apart from serving I2C requests) between the getPosition() call above and
      the getDirection() call in the line below, both results will mismatch. Direction could be
@@ -74,7 +83,7 @@ void loop()
      up here, to reduce that probability, yet it cannot be fully prevented. Make your code robust
      against this possibility.
   */
-  
+
   RotaryEncoder::Direction dir = encoder.getDirection();
   if (pos != newPos) {
     Serial.print("pos:"); Serial.print(newPos);
@@ -85,6 +94,19 @@ void loop()
 
   }
   delay(20); // don't clog the I2C bus
+}
+
+void loopDiagnostics()
+{
+  while (true) {
+    uint8_t pinstates = encoder.getDiagnostics();
+    if (pinstates != 0xFF) {
+      Serial.print(pinstates & 0x1); Serial.print(" ");  Serial.println((pinstates & 0x2) >> 1);
+    } else {
+      Serial.println("getDiagnostics() error");
+    }
+    //delay(10);
+  }
 }
 
 
